0bc86139289d1ebddc720418cf7952b7acdd243f
Aki  Väliaho <aki.valiaho@digia.com>
2019-08-16 11:02:23 +0300

    Rail tripupdate source

---
+++ src/main/java/fi/hsl/transitdata/railsource/HSLRailSourceMain.java
@@ -0,0 +1,63 @@
+package fi.hsl.transitdata.railsource;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.typesafe.config.Config;
+import fi.hsl.common.config.ConfigParser;
+import fi.hsl.common.pulsar.PulsarApplication;
+import fi.hsl.common.pulsar.PulsarApplicationContext;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Rail source service sends tripupdates and alerts from trains
+ * to corresponding tripupdate and alert topics.
+ */
+public class HSLRailSourceMain {
+
+    private static final Logger log = LoggerFactory.getLogger(HSLRailSourceMain.class);
+
+    public static void main(String[] args) {
+
+        try {
+            final Config config = ConfigParser.createConfig();
+            final PulsarApplication app = PulsarApplication.newInstance(config);
+            final PulsarApplicationContext context = app.getContext();
+            final HslRailPoller poller = new HslRailPoller(context.getProducer(), context.getJedis(), config,
+                    new RailTripUpdateService(context.getProducer(), context.getJedis(), config.getString("serviceDayStartTime")));
+
+            final int pollIntervalInSeconds = config.getInt("poller.interval");
+            final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+            scheduler.scheduleAtFixedRate(() -> {
+                try {
+                    poller.poll();
+                } catch (InvalidProtocolBufferException e) {
+                    log.error("Cancellation message format is invalid", e);
+                } catch (PulsarClientException e) {
+                    log.error("Pulsar connection error", e);
+                    closeApplication(app, scheduler);
+                } catch (IOException e) {
+                    log.error("Error with HTTP connection: " + e.getMessage(), e);
+                } catch (Exception e) {
+                    log.error("Unknown exception at poll cycle: ", e);
+                    closeApplication(app, scheduler);
+                }
+            }, 0, pollIntervalInSeconds, TimeUnit.SECONDS);
+
+
+        } catch (Exception e) {
+            log.error("Exception at fi.hsl.transitdata.railsource.HSLRailSourceMain: " + e.getMessage(), e);
+        }
+    }
+
+    private static void closeApplication(PulsarApplication app, ScheduledExecutorService scheduler) {
+        log.warn("Closing application");
+        scheduler.shutdown();
+        app.close();
+    }
+}
---
+++ src/main/java/fi/hsl/transitdata/railsource/HslRailPoller.java
@@ -0,0 +1,61 @@
+package fi.hsl.transitdata.railsource;
+
+import com.google.transit.realtime.GtfsRealtime;
+import com.typesafe.config.Config;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClientException;
+import redis.clients.jedis.Jedis;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+@Slf4j
+class HslRailPoller {
+
+    private final Producer<byte[]> producer;
+    private final Jedis jedis;
+    private final String serviceDayStartTime;
+    private final String railUrlString;
+    private final RailTripUpdateService railTripUpdateService;
+
+    HslRailPoller(Producer<byte[]> producer, Jedis jedis, Config config, RailTripUpdateService railTripUpdateService) {
+        this.railUrlString = config.getString("poller.railurl");
+        this.producer = producer;
+        this.jedis = jedis;
+        this.serviceDayStartTime = config.getString("poller.serviceDayStartTime");
+        this.railTripUpdateService = railTripUpdateService;
+    }
+
+    void poll() throws IOException {
+        GtfsRealtime.FeedMessage feedMessage = readFeedMessage(railUrlString);
+        handleFeedMessage(feedMessage);
+    }
+
+    static GtfsRealtime.FeedMessage readFeedMessage(String url) throws IOException {
+        return readFeedMessage(new URL(url));
+    }
+
+    static GtfsRealtime.FeedMessage readFeedMessage(URL url) throws IOException {
+        log.info("Reading rail feed messages from " + url);
+
+        try (InputStream inputStream = url.openStream()) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+            byte[] readWindow = new byte[256];
+            int numberOfBytesRead;
+
+            while ((numberOfBytesRead = inputStream.read(readWindow)) > 0) {
+                byteArrayOutputStream.write(readWindow, 0, numberOfBytesRead);
+            }
+            return GtfsRealtime.FeedMessage.parseFrom(byteArrayOutputStream.toByteArray());
+        }
+    }
+
+    private void handleFeedMessage(GtfsRealtime.FeedMessage feedMessage) throws PulsarClientException {
+        railTripUpdateService.sendRailTripUpdates(feedMessage);
+    }
+
+}
