618cc25c0a30f94a4ace3b44e1a65e0686a9dd41
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2019-08-22 13:03:25 +0300

    Fix tests

--- src/test/java/fi/hsl/transitdata/railsource/RailTripUpdateServiceTest.java
+++ src/test/java/fi/hsl/transitdata/railsource/RailTripUpdateServiceTest.java
@@ -5,11 +5,17 @@ import fi.hsl.common.pulsar.PulsarApplicationContext;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.client.api.Producer;
 import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.TypedMessageBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.CompletableFuture;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @Slf4j
 public class RailTripUpdateServiceTest {
@@ -22,12 +28,23 @@ public class RailTripUpdateServiceTest {
     @Before
     public void init() {
         FEEDMESSAGE = TestUtils.readExample();
+
         Producer producerMock = mock(Producer.class);
+
+        TypedMessageBuilder typedMessageBuilderMock = mock(TypedMessageBuilder.class);
+        when(typedMessageBuilderMock.key(any())).thenReturn(typedMessageBuilderMock);
+        when(typedMessageBuilderMock.value(any())).thenReturn(typedMessageBuilderMock);
+        when(typedMessageBuilderMock.eventTime(anyLong())).thenReturn(typedMessageBuilderMock);
+        when(typedMessageBuilderMock.property(any(), any())).thenReturn(typedMessageBuilderMock);
+        when(typedMessageBuilderMock.sendAsync()).thenReturn(CompletableFuture.completedFuture(null));
+
+        when(producerMock.newMessage()).thenReturn(typedMessageBuilderMock);
+
         this.railTripUpdateService = new RailTripUpdateService(producerMock);
     }
 
     @Test
-    public void handleRailAlerts_sendValidAlert_shouldSendToProducer() throws PulsarClientException {
+    public void handleRailAlerts_sendValidAlert_shouldSendToProducer() {
         Integer sentTripUpdates = this.railTripUpdateService.sendRailTripUpdates(FEEDMESSAGE);
         //Example file contains 35 alerts
         assertEquals(sentTripUpdates, 232, 0);
